48522c0161b7a8ef2a08574740c5649adb8fa9a3
AMALGSOFT <rivangushu200241>
2023-09-05 10:08:25 +0300

    first commit

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render,redirect
 
-from listings.choices import price_choices , bedroom_choices , state_choices
+from listings.choices import price_choices ,bedroom_choices , state_choices
 from listings.models import Listing
 from realtors.models import Realtor
 
 # Create your views here.
 def index(request):
 	listings = Listing.objects.order_by('-list_date').filter(is_published=True);
-	return render(request , 'pages/index.html',{
-     	'listings' : listings ,
+	return render(request ,'pages/index.html',{
+     	'listings' : listings,
         'state_choices' : state_choices,
         'bedroom_choices' : bedroom_choices,
         'price_choices' : price_choices,
